b8e1b32e170f427d3032270bb22809575537b3f7
69kat69 <jalvarezb2401@alumno.ipn.mx>
2026-05-09 01:07:48 -0600

    reponiendo repositorio borrado

---
+++ 4IV9_Alvarez_Bastidas_Juan_Luis/05Clases/Principal.java
@@ -0,0 +1,9 @@
+class Principal{
+
+    //metodo principal
+    public static void main(String[] args){
+        //un ejercicio de figuritas
+        ejercicios1 ejecucion = new ejercicios1();
+        ejecucion.menu();
+    }
+}
