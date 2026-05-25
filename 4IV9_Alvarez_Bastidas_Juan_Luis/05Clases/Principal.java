fe01d66f4e924734aa8da63d6a0a2796b0344aa3
69kat69 <jalvarezb2401@alumno.ipn.mx>
2026-05-25 16:23:53 -0600

    13Ventana ejemplo

--- 4IV9_Alvarez_Bastidas_Juan_Luis/05Clases/Principal.java
+++
@@ -1,9 +0,0 @@
-class Principal{
-
-    //metodo principal
-    public static void main(String[] args){
-        //un ejercicio de figuritas
-        ejercicios1 ejecucion = new ejercicios1();
-        ejecucion.menu();
-    }
-}
